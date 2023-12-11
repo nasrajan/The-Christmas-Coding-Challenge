ca86a8483f0c5086bd955dea2a0c38e812182a76
nasrajan <nasrajan.jalin@sjsu.edu>
2023-12-10 23:13:29 -0800

    Day 9 - Leetcode problem 12 - Integer to Roman

---
+++ DSA/src/main/java/DSA/LeetCode/IntegerToRoman.java
@@ -0,0 +1,69 @@
+package DSA.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IntegerToRoman {
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+        while (num > 0) {
+            if (num >= 1000) {
+                sb.append(map.get(1000));
+                num = num - 1000;
+            } else if (num >= 900) {
+                sb.append(map.get(900));
+                num -= 900;
+            } else if (num >= 500) {
+                sb.append(map.get(500));
+                num -= 500;
+            }else if (num >= 400) {
+                sb.append(map.get(400));
+                num = num - 400;
+            } else if (num >= 100) {
+                sb.append(map.get(100));
+                num = num - 100;
+            } else if (num >= 90) {
+                sb.append(map.get(90));
+                num -= 90;
+            } else if (num >= 50) {
+                sb.append(map.get(50));
+                num -= 50;
+            } else if (num >= 40) {
+                sb.append(map.get(40));
+                num -= 40;
+            } else if (num >= 10) {
+                sb.append(map.get(10));
+                num = num - 10;
+            } else if(num >=9) {
+                sb.append(map.get(9));
+                num = num - 9;
+            } else if (num >= 5) {
+                sb.append(map.get(5));
+                num -= 5;
+            } else if (num >= 4) {
+                sb.append(map.get(4));
+                num -= 4;
+            }else {
+                sb.append(map.get(1));
+                num -= 1;
+            }
+        }
+
+        return sb.toString();
+    }
+}
